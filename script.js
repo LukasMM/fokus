134b19e21dd561de64ba01e7b013b699c2e900b9
Lucas Mazini <lukasloko71@hotmail.com>
2026-09-05 21:03:03 -0300

    fix the bugs

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ const iniciarOuPausarBtIcone = document.querySelector(
 
 const tempoNaTela = document.querySelector("#timer");
 
-const musica = new Audio("/sons/luna-rise-part-one.mp3");
-const audioPlay = new Audio("/sons/play.wav");
-const audioPausa = new Audio("/sons/pause.mp3");
+const musica = new Audio("./sons/luna-rise-part-one.mp3");
+const audioPlay = new Audio("./sons/play.wav");
+const audioPausa = new Audio("./sons/pause.mp3");
 const audioTempoFinalizado = new Audio("./sons/beep.mp3");
 
 let tempoDecorridoEmSegundos = 1500;
@@ -61,7 +61,7 @@ function alterarContexto(contexto) {
   });
 
   html.setAttribute("data-contexto", contexto);
-  banner.setAttribute("src", `/imagens/${contexto}.png`);
+  banner.setAttribute("src", `./imagens/${contexto}.png`);
 
   switch (contexto) {
     case "foco":
@@ -116,14 +116,14 @@ function iniciarOuPausar() {
   intervaloId = setInterval(contagemRegressiva, 1000);
 
   iniciarOuPausarBt.textContent = "Pausar";
-  iniciarOuPausarBtIcone.setAttribute("src", `/imagens/pause.png`);
+  iniciarOuPausarBtIcone.setAttribute("src", `./imagens/pause.png`);
 }
 
 function zerar() {
   clearInterval(intervaloId);
 
   iniciarOuPausarBt.textContent = "Começar";
-  iniciarOuPausarBtIcone.setAttribute("src", `/imagens/play_arrow.png`);
+  iniciarOuPausarBtIcone.setAttribute("src", `./imagens/play_arrow.png`);
 
   intervaloId = null;
 }
